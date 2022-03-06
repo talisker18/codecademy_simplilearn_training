580daf61874758c728e94abc885090b6edcd9e64
talisker18 <hjoel87@gmx.ch>
2022-03-06 16:06:39 +0100

    .

--- src/main/java/AAD_java8/streams/training/K_IntSummaryStatistics.java
+++ src/main/java/AAD_java8/streams/training/K_IntSummaryStatistics.java
@@ -6,7 +6,7 @@ import java.util.stream.IntStream;
 public class K_IntSummaryStatistics {
 	
 	public static void main(String[] args) {
-		IntSummaryStatistics summary = IntStream.of(7,2,19,88,73,4,10)
+		IntSummaryStatistics summary = IntStream.of(7,2,19,88,73,4,10) //only works for int
 				.summaryStatistics();
 		
 		System.out.println(summary);
